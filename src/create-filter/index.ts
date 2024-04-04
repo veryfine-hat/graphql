ac41cb8f4d880e5b956587a68f32695dee4098a5
Bryan Pease <bcp015000@gmail.com>
2024-04-04 08:55:06 -0500

    Fixed build error in barrel file

--- src/create-filter/index.ts
+++ src/create-filter/index.ts
@@ -1,4 +1,4 @@
-export { byPredicate } from "./by-predicate";
+export { property } from "./property";
 export { intersectionOf } from './intersection-of';
 export { is } from "./is";
 export { isOneOf } from './is-one-of'
